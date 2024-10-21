89c019ff2148b913f72c7baeabd9e51a9f14b662
abdul-halim-dev <jamesborn1518@gmail.com>
2024-10-21 13:43:21 +0600

    it modified

---
+++ src/About.jsx
@@ -0,0 +1,13 @@
+ 
+
+const About = () => {
+  return (
+    <div>
+        <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus neque placeat autem mollitia quibusdam magni? Saepe maxime harum error consectetur?</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet possimus voluptas, vel quas illo explicabo maxime. Maxime, accusantium? Sunt, tenetur!</p>
+
+    </div>
+  )
+}
+
+export default About
